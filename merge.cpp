4ba392ab7796287fe49fce909533c6e03c72bb6b
Amey Mohole <112307792+AmeyMohole@users.noreply.github.com>
2025-04-28 09:33:59 +0530

    Add files via upload

--- merge.cpp
+++ merge.cpp
@@ -4,115 +4,105 @@
 
 using namespace std;
 
-void marge(int a[], int i1, int j1, int i2, int j2)
+void merge(int a[], int i1, int j1, int i2, int j2)
 {
-  int i = i1;
-  int j = i2;
-  int temp[1000000];
-  int k = 0;
-  
-  // Merge the two halves into temp[]
-  while (i <= j1 && j <= j2)
-  {
-    if (a[i] < a[j])
+    int i = i1;
+    int j = i2;
+    int* temp = new int[j2 - i1 + 1];
+    int k = 0;
+    
+    while (i <= j1 && j <= j2)
     {
-      temp[k++] = a[i++];
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
     }
-    else
-    {
-      temp[k++] = a[j++];
-    }
-  }
-  
-  while (i <= j1)
-  {
-    temp[k++] = a[i++];
-  }
-  
-
-  while (j <= j2)
-  { 
-    temp[k++] = a[j++];
-  }
-  
-  for (i = i1, j = 0; i <= j2; i++, j++)
-  {
-    a[i] = temp[j];
-  }
+    
+    while (i <= j1)
+        temp[k++] = a[i++];
+    
+    while (j <= j2)
+        temp[k++] = a[j++];
+    
+    for (i = i1, k = 0; i <= j2; i++, k++)
+        a[i] = temp[k];
+    
+    delete[] temp;
 }
 
-void margesort(int a[], int i, int j)
+void mergeSort_seq(int a[], int i, int j)
 {
-  int mid;
-  if (i < j)
-  {
-    mid = (i + j) / 2;
-    
-    #pragma omp parallel sections
+    if (i < j)
     {
-      #pragma omp section
-      margesort(a, i, mid);
-      
-      #pragma omp section
-      margesort(a, mid + 1, j);
+        int mid = (i + j) / 2;
+        mergeSort_seq(a, i, mid);
+        mergeSort_seq(a, mid + 1, j);
+        merge(a, i, mid, mid + 1, j);
     }
-    
-    marge(a, i, mid, mid + 1, j);
-  }
 }
 
-void margeSort_seq(int a[], int i, int j)
+void mergeSort(int a[], int i, int j)
 {
-  int mid;
-  if (i < j)
-  {
-    mid = (i + j) / 2;
-    
-    margeSort_seq(a, i, mid);
-    margeSort_seq(a, mid + 1, j);
-    
-    marge(a, i, mid, mid + 1, j);
-  }
+    if (i < j)
+    {
+        if ((j - i) < 1000) // cutoff for small arrays
+        {
+            mergeSort_seq(a, i, j);
+            return;
+        }
+
+        int mid = (i + j) / 2;
+        
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            mergeSort(a, i, mid);
+            
+            #pragma omp section
+            mergeSort(a, mid + 1, j);
+        }
+        
+        merge(a, i, mid, mid + 1, j);
+    }
 }
 
 int main()
 {
-  int *a, *b, n = 100000;
-  
-  a = new int[n];
-  b = new int[n];
-  
-  for (int i = 0; i < n; i++)
-  {
-    a[i] = rand() % 100000;
-    b[i] = a[i];
-  }
-  
-  
-  double start = omp_get_wtime();
-  margesort(a, 0, n - 1);
-  double end = omp_get_wtime();
-  
-  double start1 = omp_get_wtime();
-  margeSort_seq(b, 0, n - 1);
-  double end1 = omp_get_wtime();
-  
-   cout << "Sorted array (Parallel Merge Sort):" << endl;
-  for (int i = 0; i < 100; i++)
+    int *a, *b, n = 100000;
+    
+    a = new int[n];
+    b = new int[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 100000;
+        b[i] = a[i];
+    }
+    
+    double start1 = omp_get_wtime();
+    mergeSort_seq(b, 0, n - 1);
+    double end1 = omp_get_wtime();
+    
+    double start = omp_get_wtime();
+    mergeSort(a, 0, n - 1);
+    double end = omp_get_wtime();
+    
+    cout << "Sorted array (Parallel Merge Sort):" << endl;
+    for (int i = 0; i < 100; i++)
     {
         cout << a[i] << "\t";
         if ((i + 1) % 10 == 0)
             cout << endl;
     }
 
-  cout << endl;
-  
-  cout << "Time for Parallel Merge Sort: " << (end - start) << " seconds" << endl;
-  cout << "Time for Sequential Merge Sort: " << (end1 - start1) << " seconds" << endl;
-  
-  delete[] a;
-  delete[] b;
+    cout << endl;
+    
+    cout << "Time for Sequential Merge Sort: " << (end1 - start1) << " seconds" << endl;
+    cout << "Time for Parallel Merge Sort: " << (end - start) << " seconds" << endl;
+    
+    delete[] a;
+    delete[] b;
 
-  return 0;
+    return 0;
 }
-
